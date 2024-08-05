38585119522c22608ec6b10d7b287872e78821df
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-05 14:20:12 -0400

    MRR2024: S2. Creating Content with JSX L10. Showing Basic Content

---
+++ jsx/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+function App() {
+  return <h1> Hi There!</h1>;
+}
+
+root.render(<App />);
