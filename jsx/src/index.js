241d4fbdaa051cea29cc7d5c9f0f7d0f4f32b006
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-05 14:28:15 -0400

    MRR2024: S2. Creating Content with JSX L12. Printing JS Variables in JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -7,7 +7,13 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 function App() {
-  return <h1> Hi There!</h1>;
+  // React cannot properly print out bool, null, undefined, arrays, and objects
+  let message = 'Bye there!';
+  if (Math.random() > 0.5) {
+    message = 'Hello There!';
+  }
+
+  return <h1>{message}</h1>;
 }
 
 root.render(<App />);
