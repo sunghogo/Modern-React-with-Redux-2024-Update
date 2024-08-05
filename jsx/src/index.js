7683d9b473158369087097cabfba36c36cbf3c6f
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-05 14:39:56 -0400

    MRR2024: S2. Creating Content with JSX L16. Typical Component Layouts

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,16 +1,35 @@
 // babeljs.io/repl : To see transpiled JSX code
 
+// 1) Import the React and the ReatctDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
+
+// 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
+// 4) Create a component
 function App() {
-  // React cannot properly print out bool, null, undefined, arrays, and objects
-  // React can evaluates expressions in JSX
+  /*
+  - React can interpolate variable values inside curly braces {}
+  - React cannot properly print out bool, null, undefined, arrays, and objects
+  - React can evaluates expressions in JSX
+  - Component Layout typically has code to: 
+    1) compute values we want to show in our JSX
+    2) returns the content we want this component to show
+  */
 
-  return <h1>{new Date().toLocaleDateString()}</h1>;
+  const name = 'Samantha';
+  const age = 23;
+
+  return (
+    <h1>
+      Hi, my name is {name} and my age is {age}
+    </h1>
+  );
 }
 
+// 5) Show the component on screen
 root.render(<App />);
