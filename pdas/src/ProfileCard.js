982546b1fe9e07b8317efc041924a8c7ac8407c7
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-08 12:29:44 -0400

    MRR2024: S3: Building with Reusable Components: L38 A Big Pile of HTML!

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -2,10 +2,19 @@ function ProfileCard({ title, handle, image }) {
   // Destructure objects in code OR directly in arguments since props object is always passed
 
   return (
-    <div>
-      <img src={image} alt="pda logo" />
-      <div>Title is {title}</div>
-      <div>Handle is {handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is=1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <div className="title is-4">Title is {title}</div>
+          <div className="subtitle is-6">Handle is {handle}</div>
+        </div>
+      </div>
     </div>
   );
 }
