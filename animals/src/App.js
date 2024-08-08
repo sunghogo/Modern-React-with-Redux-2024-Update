50b6624e094cdb8ca639015aa7720a3b67e3202f
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-08 13:03:41 -0400

    MRR2024: S4: State How to Change your App 43: Events in Detail

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,5 +1,26 @@
 function App() {
-  return <div>Show animal list here!</div>;
+  /*
+    - Event System (user input) uand State System (updates to pages)
+    - Using Events:
+      1) Decide what kind of event
+      2) Create event handler/callback function
+      3) Name the function using handle + EventNAme
+      4) Pass handler function as a prop to a plain element (Don't call it!)
+      5) Use valid prop event name ('onClick', 'onChange', 'onMouseOver', etc.)
+
+    - Event Documentation:
+      reactjs.org/docs/events.html
+  */
+
+  const handleClick = () => {
+    console.log('Button was clicked!');
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+    </div>
+  );
 }
 
 export default App;
