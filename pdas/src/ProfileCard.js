87620b314619eb5dec7fbcfd278508e89c6b115a
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-08 12:14:49 -0400

    MRR2024: S3: Building with Reusable Components: L35. Handling Image Accessibility

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,8 +1,9 @@
-function ProfileCard({ title, handle }) {
+function ProfileCard({ title, handle, image }) {
   // Destructure objects in code OR directly in arguments since props object is always passed
 
   return (
     <div>
+      <img src={image} alt="pda logo" />
       <div>Title is {title}</div>
       <div>Handle is {handle}</div>
     </div>
