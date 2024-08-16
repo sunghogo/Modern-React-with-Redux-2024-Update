edb7c46ded9e3a948d71c83c035c918ef4ce7b83
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-16 15:52:17 -0400

    MRR2024: S5: Using an API with React L79. Handling List Updates

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,5 +1,27 @@
+/*
+    Applying (State) Update to Lists:
+        A) Delete and recreate entire list
+            - Can be computationally expensive for long lists
+        B) Modify existing list
+            - Apply a "Key" to each element during mapping
+            - Apply minimal set of change sto existing DOM elements
+    
+    Steps for Applying Update (Option B):
+        0) Apply a "Key" to each elemnt during mapping step (We do this)
+            - If we don't give key prop to elements in a list, React will show an error
+        1) After re-rendering, compare the keys on each ImageShow to the keys from the previous render (React does this)
+        2) Apply minimal set of changes to existing DOM elements (React does this)
+    
+*/
+
+import ImageShow from './ImageShow';
+
 function ImageList({ images }) {
-  return <div>ImageList: {images.length}</div>;
+  const renderedImages = images.map((image) => {
+    return <ImageShow image={image} />;
+  });
+
+  return <div>{renderedImages}</div>;
 }
 
 export default ImageList;
