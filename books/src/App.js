291a9030996dd21c2d4f8ab12fb530fff1b8041b
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-16 16:44:30 -0400

    MRR2024: S5: How to Handle Forms L91. Updating State

--- books/src/App.js
+++ books/src/App.js
@@ -1,3 +1,13 @@
+/*
+    React Rendering with Arrays:
+        - If state point to the same array/object.memory, React is optimized to assume no rerender is required!
+        - To fix this:
+            1) Create a new array
+            2) Copy all elements from old array
+            3) Add new element to the end
+            4) Set the sate to new array
+*/
+
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
 
@@ -5,7 +15,8 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log('Need to add book with: ', title);
+    const book = { title };
+    setBooks([...books, book]);
   };
 
   return (
