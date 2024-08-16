24ddba15d6b01736f7c59e7a680f46489bcc1223
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-16 15:40:46 -0400

    MRR2024: S5: Using an API with React L77. Communicating the List of Images Down

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,14 +1,28 @@
+/*
+    State as Props:
+        - When you update state, the component and ALL OF ITS CHILDREN are rerendered
+        - From perspective of parent component, 'images' is state
+        - From the perspective of child component, 'images' is prop
+*/
+
+import { useState } from 'react';
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 import searchImages from './api';
 
 function App() {
+  const [images, setImages] = useState([]);
+
   const handleSubmit = async (term) => {
     // const result = await searchImages(term);
+    const result = [];
+    setImages(result);
   };
 
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
     </div>
   );
 }
