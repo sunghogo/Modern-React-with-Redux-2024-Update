1194aed6268628b86171a7dc2b62944453d35db6
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-16 15:05:02 -0400

    MRR2024: S5: Using an API with React L71. Handling Form Submission

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,12 +1,22 @@
+/*
+  Form behavior:
+      - Automatically sends input data to homeUrl?input1Name=enteredData1&input2Name=enteredData2&etc..
+        - Input data can be pass by pressing Enter key or an included form button
+      - Pass the event argument in handler function and call event.preventDefault(); to prevent this behavior
+*/
+
 function SearchBar({ onSubmit }) {
-  const handleClick = () => {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
     onSubmit('cars');
   };
 
   return (
     <div>
-      <input />
-      <button onClick={handleClick}>Click me</button>
+      <form onSubmit={handleFormSubmit}>
+        <input />
+      </form>
     </div>
   );
 }
