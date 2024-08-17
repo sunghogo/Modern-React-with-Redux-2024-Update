ba5a63244ba5cd34cbf4788bcd27137947bab242
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-17 11:04:30 -0400

    MRR2024: S5: How to Handle Forms L108. Deleting Records

--- books/src/App.js
+++ books/src/App.js
@@ -20,12 +20,25 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    setBooks([...books, { id: Math.round(Math.random() * 99999), title }]);
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 99999), title },
+    ];
+
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
   };
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
