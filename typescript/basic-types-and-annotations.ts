50f33f6bdef21936fec0c8f180bf347be6f268eb
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-20 13:06:00 -0400

    Section 23: Diving into TypeScript L415. Basic Types and Type Annotations

---
+++ typescript/basic-types-and-annotations.ts
@@ -0,0 +1,21 @@
+/*
+    - Typescript:
+        - Helps you catch errors during development
+        - Helps provide documenation for your components
+        - Compiles to plain Javascript
+        - Doesn't improve performance
+
+    - Type annotations helps TS understand the data flowing through our app
+    - Basic Types:
+        - string, string[]
+        - number, number[]
+        - boolean, boolean[]
+*/
+
+const color: string = 'red';
+const isRed: boolean = color === 'red';
+const age: number = 25;
+
+const colors: string[] = ['red', 'green', 'blue'];
+const areColorsRed: boolean[] = [true, false, false];
+const ages: number[] = [20, 30, 40];
