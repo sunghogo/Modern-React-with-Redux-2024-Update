2f255ffd26df13b57736d00e5f60be9aa7e63fc8
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-20 12:30:51 -0400

    MRR2024: S10: Custom Navigation and Routing Systems L158. The Children Prop

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,5 +1,29 @@
+/*
+    - Components recreating their html element equivalent are called "wrappers", and the plain element as the "underlying element"
+    - In React, anything between the opening and closing component JSX, and is automatically passed down as the "children" prop
+*/
+import Button from './Button';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button>Click!!</Button>
+      </div>
+      <div>
+        <Button>Buy Now!</Button>
+      </div>
+      <div>
+        <Button>See Deal!</Button>
+      </div>
+      <div>
+        <Button>Hide Ads!</Button>
+      </div>
+      <div>
+        <Button>Something!</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
