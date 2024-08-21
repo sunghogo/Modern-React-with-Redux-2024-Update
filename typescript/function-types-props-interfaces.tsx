89b48107e6df4ecda3b5c1e2ee5bf6631be967c8
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-21 12:55:24 -0400

    Section 23: Diving into TypeScript L422. Function Types in Props Interfaces

---
+++ typescript/function-types-props-interfaces.tsx
@@ -0,0 +1,27 @@
+/*
+    - Since functions/methods are annotated within interfaces, can rely on type inference when using those methods
+*/
+
+import React from 'react';
+
+interface ColorPickerProps {
+  colors: string[];
+  handleColorSelect: (color: string) => void;
+}
+function ColorPicker({ colors, handleColorSelect }: ColorPickerProps) {
+  const renderedColors = colors.map((color) => {
+    return (
+      <button key={color} onClick={() => handleColorSelect(color)}>
+        {color}
+      </button>
+    );
+  });
+
+  return <div>{renderedColors}</div>;
+}
+
+<ColorPicker
+  colors={['red', 'green', 'blue']}
+  //   handleColorSelect={(color: string) => console.log(color)}
+  handleColorSelect={(color) => console.log(color)}
+/>;
