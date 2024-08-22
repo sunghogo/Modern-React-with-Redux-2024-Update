940e4a90f1502fb19ac8abb737b80cf1bc738790
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-22 12:48:09 -0400

    MRR2024: Section 23: Diving into TypeScript L433. More on Function Generics refactored generics lesson

--- typescript/function-generics.ts
+++ typescript/function-generics.ts
@@ -1,5 +1,10 @@
 /*
+    Limitations without using generics: 
+        - Using type unions/aliases causes return type ambiguity without type narrowing
+        - Creating new functions to deal with each type can get unmaintainable
+
     Generics:
+        - Makes it easier to write functions, interfaces, and more that work with multiple different types
         - Work like arguments but specifically to configure types within a function
             - Naming convention uses 'T' as the type identifier
 
@@ -7,6 +12,22 @@
             ...
         }
 */
+
+// function wrapInArray(value: string | number): (string | number)[] {
+//   return [value];
+// }
+
+// function wrapInArray(value: string): string[] {
+//   return [value];
+// }
+
+// function wrapNumberInArray(value: number): number[] {
+//   return [value];
+// }
+
+// const result = wrapInArray('someString');
+// const result2 = wrapInArray(10);
+// const result3 = wrapNumberInArray(10);
 
 function wrapInArray<T>(value: T): T[] {
   return [value];
