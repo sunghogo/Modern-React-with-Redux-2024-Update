d66f234b9bb111e670f4190282eaef9258036279
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-22 12:44:34 -0400

    MRR2024: Section 23: Diving into TypeScript L433. More on Function Generics

--- typescript/function-generics.ts
+++ typescript/function-generics.ts
@@ -8,9 +8,16 @@
         }
 */
 
-function wrapInArray<TypeToWrap>(value: TypeToWrap): TypeToWrap[] {
+function wrapInArray<T>(value: T): T[] {
   return [value];
 }
 
 const result4 = wrapInArray<string>('someString');
 const result5 = wrapInArray<number>(10);
+
+function toRecord<TypeOfId>(id: TypeOfId, value: string) {
+  return { id, value };
+}
+
+const result6 = toRecord<number>(123, 'my@email.com');
+const result7 = toRecord<string>('ea01', 'other@email.com');
