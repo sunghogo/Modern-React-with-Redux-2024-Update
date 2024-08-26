08390dd3515f1e0f22d3f604f828201a809c2699
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-26 13:57:06 -0400

    MRR2024: Section 10: Custom Navigation and Routing Systems: L166. Review on Styling

--- comps/src/Button.tsx
+++ comps/src/Button.tsx
@@ -28,7 +28,11 @@ const Button: FunctionComponent<ButtonProps> = ({
   rounded,
   ...rest
 }) => {
-  return <button>{children}</button>;
+  return (
+    <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white ">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
