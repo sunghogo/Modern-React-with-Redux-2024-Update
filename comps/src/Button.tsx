7b4f17d749b488470540c1f47b32cf99dbd15bfb
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-26 14:12:04 -0400

    MRR2024: Section 10: Custom Navigation and Routing Systems: L169. text-white Overriding Other Colors

--- comps/src/Button.tsx
+++ comps/src/Button.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import { twMerge } from 'tailwind-merge';
 import className from 'classnames';
 
 type ExcludeFromTuple<T extends any[], U> = {
@@ -29,13 +30,22 @@ const Button: FunctionComponent<ButtonProps> = ({
   rounded,
   ...rest
 }) => {
-  const classes = className('px-3 py-1.5 border', {
-    'border-blue-500 bg-blue-500 text-white': primary,
-    'border-gray-900 bg-gray-900 text-white': secondary,
-    'border-green-500 bg-green-500 text-white': success,
-    'border-yellow-400 bg-yellow-400 text-white': warning,
-    'border-red-500 bg-red-500 text-white': danger,
-  });
+  const classes = twMerge(
+    className('px-3 py-1.5 border', {
+      'border-blue-500 bg-blue-500 text-white': primary,
+      'border-gray-900 bg-gray-900 text-white': secondary,
+      'border-green-500 bg-green-500 text-white': success,
+      'border-yellow-400 bg-yellow-400 text-white': warning,
+      'border-red-500 bg-red-500 text-white': danger,
+      'rounded-full': rounded,
+      'bg-white': outline,
+      'text-blue-500': outline && primary,
+      'text-gray-900': outline && secondary,
+      'text-green-500': outline && success,
+      'text-yellow-400': outline && warning,
+      'text-red-500': outline && danger,
+    })
+  );
   return <button className={classes}>{children}</button>;
 };
 
