371d5e3fe00c99e461649fbf99f596db04c98e14
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-08-27 10:56:28 -0400

    MRR2024: Section 11: Mastering the State Design L179. Component Setup

--- comps/src/App.js
+++ comps/src/App.js
@@ -11,10 +11,27 @@
       - Page: Still a React component but not intended to be reused
 
 */
-import ButtonPage from './pages/ButtonPage';
+import Accordion from './components/Accordion';
 
 function App() {
-  return <ButtonPage />;
+  const items = [
+    {
+      label: 'Can I use React on a project?',
+      content:
+        'You can use React on any project you want. You can use React on any project you want',
+    },
+    {
+      label: 'Can I use JavaScript on a project?',
+      content:
+        'You can use JavaScript on any project you want. You can use JavaScript on any project you want',
+    },
+    {
+      label: 'Can I use CSS on a project?',
+      content:
+        'You can use CSS on any project you want. You can use CSS on any project you want',
+    },
+  ];
+  return <Accordion items={items} />;
 }
 
 export default App;
