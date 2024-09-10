ce54330e68c70bc563c25775bc504216c103d78a
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-09-10 13:08:58 -0400

    MRR2024: Section 12: Practicing Prop and State Design L206. Adding Styling

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -6,6 +7,7 @@ function Dropdown({ options, value, onChange }) {
   const renderedOptions = options.map((option) => {
     return (
       <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
         key={option.value}
         onClick={() => {
           onChange(option);
@@ -18,11 +20,23 @@ function Dropdown({ options, value, onChange }) {
   });
 
   return (
-    <div>
-      <div onClick={() => setIsOpen((currentIsOpen) => !currentIsOpen)}>
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={() => setIsOpen((currentIsOpen) => !currentIsOpen)}
+      >
         {value?.label || 'Select...'}
+        {isOpen ? (
+          <GoChevronDown className="text-lg" />
+        ) : (
+          <GoChevronLeft className="text-lg" />
+        )}
       </div>
-      {isOpen && <div>{renderedOptions}</div>}
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
     </div>
   );
 }
